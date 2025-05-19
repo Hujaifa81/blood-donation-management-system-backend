76716cc3fdc01c52d8fe43b1bf6cb88a794ec671
Hujaifa81 <abuhojaifa123@gmail.com>
2025-05-20 02:53:33 +0600

    user get and post api completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const secretKey = process.env.JWT_SECRET;
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','https://blood-donation-managemen-7ebd3.web.app'],
   credentials: true,
 }));
 app.use(express.json());
@@ -51,59 +51,173 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // await client.connect();
-    const usersCollection=client.db('blood_donation').collection('users');
+    const usersCollection = client.db('blood_donation').collection('users');
+    const donationRequestsCollection = client.db('blood_donation').collection('donationRequests')
 
     // Verify JWT token
     app.post('/jwt', async (req, res) => {
-      const user=req.body;
-      const token=jwt.sign(user, secretKey, { expiresIn: '1h' });
+      const user = req.body;
+      const token = jwt.sign(user, secretKey, { expiresIn: '1h' });
       res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       })
-      .send({success:true});
+        .send({ success: true });
 
     })
+    //get single user by email
+    app.get('/user/:email',async (req,res)=>{
+      const email=req.params.email
+      const query={email}
+      const result=await usersCollection.findOne(query)
+      res.send(result)
+    })
     // Create a new user
-    app.post('/users/:email',async(req,res)=>{
-      const email=req.params.email;
-      const user=req.body;
-      const query={email};
-      const isExist=await usersCollection.findOne(query);
-      if(isExist){
-       const updateDoc = {
-        $set: {
-        signedIn: new Date(),
-      },
-      };
-      const result = await usersCollection.updateOne(query, updateDoc)
-        return res.status(200).send({ message:'User already exists', result });
+    app.post('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email };
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        const updateDoc = {
+          $set: {
+            signedIn: new Date(),
+          },
+        };
+        const result = await usersCollection.updateOne(query, updateDoc)
+        return res.status(200).send({ message: 'User already exists', result });
       }
-      const result=await usersCollection.insertOne({
+      const result = await usersCollection.insertOne({
         ...user,
-        role:'donor',
-        status:'active',
-        createdAt:new Date(),
-        signedIn:new Date()
+        role: 'donor',
+        status: 'active',
+        createdAt: new Date(),
+        signedIn: new Date()
       });
       res.status(201).send({
-        message:'User created successfully',
+        message: 'User created successfully',
         result
       });
     })
+     //update user details
+    app.put('/user/:email',async(req,res)=>{
+      const email=req.params.email;
+      const {name,bloodGroup,district,upazila,image}=req.body
+      const query={email}
+      const updateDoc={
+        $set:{
+          name:name,
+          bloodGroup:bloodGroup,
+          district:district,
+          upazila:upazila,
+          image:image
+        }
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      return res.status(200).send({ message: 'successful', result });
+
+    })
+    //get single donation request by id
+    app.get('/donationRequest/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; // Ensure this matches your DB field
+
+      const result = await donationRequestsCollection.findOne(query);
+      res.send(result);
+    });
+   
+    //get donation request by email
+    app.get('/donationRequests/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }; // Ensure this matches your DB field
+
+      const result = await donationRequestsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //create donation request
+    app.post('/donationRequests/:email', async (req, res) => {
+      const email = req.params.email
+      const data = req.body
+      const user = await usersCollection.findOne({ email });
+      if (user.status === 'blocked') {
+        return res.status(200).send({ message: 'User is blocked', result });
+      }
+      const result = await donationRequestsCollection.insertOne({
+        ...data,
+        status: 'pending'
+      })
+      res.status(201).send({
+        message: 'Donation Request created successfully',
+        result
+      });
+    })
+    //update donation request
+    app.put('/donationRequests/:id', async (req, res) => {
+      const id = req.params.id
+      const { recipientName,recipientDistrict,donationTime,recipientUpazila,hospitalName,fullAddress,bloodGroup,donationDate} = req.body
+      const query = { _id: new ObjectId(id) }
+      let updateDoc = {
+        $set: {
+          recipientName:recipientName,
+          recipientDistrict:recipientDistrict,
+          recipientUpazila:recipientUpazila,
+          hospitalName:hospitalName,
+          fullAddress:fullAddress,
+          bloodGroup:bloodGroup,
+          donationDate:donationDate,
+          donationTime:donationTime,
+
+        },
+      };
+      
+      
+      const result = await donationRequestsCollection.updateOne(query, updateDoc)
+      return res.status(200).send({ message: 'successful', result });
+    })
+    //update request status
+    app.patch('/donationRequests/:id', async (req, res) => {
+      const id = req.params.id
+      const { status } = req.body
+      const query = { _id: new ObjectId(id) }
+      let updateDoc = {
+        $set: {
+          status: status,
+        },
+      };
+      if (status === 'inprogress') {
+         updateDoc = {
+          $set: {
+            status: status,
+            donorInfo:req.body.donorInfo
+          },
+        };
+      }
+      
+      const result = await donationRequestsCollection.updateOne(query, updateDoc)
+      return res.status(200).send({ message: 'successful', result });
+    })
+    //delete a request
+    app.delete('/donationRequests/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await donationRequestsCollection.deleteOne(query)
+      res.send({success:true})
+    })
+    //logout
     app.post('/logout', async (req, res) => {
       res.clearCookie('token', {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',// Set to true if using HTTPS
-        sameSite:process.env.NODE_ENV === 'production'?'none':'strict',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
       })
       res.send({ success: true });
     })
-    
+
 
     // // Connect the client to the server	(optional starting in v4.7)
-    
+
     // // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     // // console.log("Pinged your deployment. You successfully connected to MongoDB!");
